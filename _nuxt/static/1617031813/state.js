df6184c2c41a5266b7029980ef26a1d10ee4fb41
xiaosasori <xiaosasori@users.noreply.github.com>
2021-03-29 15:30:15 +0000

    deploy: 9e9a1d830fa7c792f9fa5e02c83db55a4cf979dd

---
+++ _nuxt/static/1617031813/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmarkdown-preview\u002F_nuxt\u002Fstatic\u002F1617031813",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fmarkdown-preview\u002F",assetsPath:"\u002Fmarkdown-preview\u002F_nuxt\u002F",cdnURL:a}}}}(null));
